6ed1c7aa015bd80a2ad160b9225b1da4617c16b2
ameen.elhoseiny <ameen.elhoseiny@aurea.com>
2017-04-08 15:31:02 +0200

    adding swagger , initiate docker

--- src/main/java/com/aurea/ca/deadcode/CaDeadcodeApplication.java
+++ src/main/java/com/aurea/ca/deadcode/CaDeadcodeApplication.java
@@ -2,27 +2,12 @@ package com.aurea.ca.deadcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import reactor.Environment;
-import reactor.bus.EventBus;
-import reactor.spring.context.config.EnableReactor;
 
 @SpringBootApplication
-@EnableReactor
 public class CaDeadcodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CaDeadcodeApplication.class, args);
     }
 
-    @Bean
-    Environment env() {
-        return Environment.initializeIfEmpty()
-            .assignErrorJournal();
-    }
-
-    @Bean
-    EventBus createEventBus(Environment env) {
-        return EventBus.create(env, Environment.THREAD_POOL);
-    }
 }
---
+++ src/main/java/com/aurea/ca/deadcode/confg/ReactorConfig.java
@@ -0,0 +1,25 @@
+package com.aurea.ca.deadcode.confg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.Environment;
+import reactor.bus.EventBus;
+import reactor.spring.context.config.EnableReactor;
+
+/**
+ * Created by ameen on 08/04/17.
+ */
+@Configuration
+@EnableReactor
+public class ReactorConfig {
+    @Bean
+    Environment env() {
+        return Environment.initializeIfEmpty()
+            .assignErrorJournal();
+    }
+
+    @Bean
+    EventBus createEventBus(Environment env) {
+        return EventBus.create(env, Environment.THREAD_POOL);
+    }
+}
